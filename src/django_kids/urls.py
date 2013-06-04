94c35c80ded9ad2560aa347f984ab8cd42b81ef8
David Grant <davidgrant@gmail.com>
2013-06-03 23:16:38 -0700

    Support for going to "Browse" view if user is not logged in and "My Links" if user is logged in.

--- src/django_kids/urls.py
+++ src/django_kids/urls.py
@@ -4,10 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
-from links.views import LinkList
+from links.views import Home
 
 urlpatterns = patterns('',
-    url(r'^$', LinkList.as_view(), name='home'),
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^l/', include('links.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
