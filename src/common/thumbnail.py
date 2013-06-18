1e0526e4e4578e2109a52c91329d3726668b8cd0
David Grant <davidgrant@gmail.com>
2013-06-18 00:08:26 -0700

    Fixed "created by: <username>" and fixed saving links (was missing an import in new "common/thumbnail" module

--- src/common/thumbnail.py
+++ src/common/thumbnail.py
@@ -1,11 +1,13 @@
 import urllib2
 from urllib2 import HTTPError
 
+import json
+import urllib2
+
 def get_youtube_thumbnail(id):
     return 'http://img.youtube.com/vi/{0}/0.jpg'.format(id)
 
 def get_vimeo_thumbnail(id):
-    import urllib2
     url = 'http://vimeo.com/api/v2/video/{0}.json'.format(id)
     try:
         openurl = urllib2.urlopen(url)
