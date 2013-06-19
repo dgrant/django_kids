854e2df5e1ec6f9e3fef7859c87e26d48155c8fd
David Grant <davidgrant@gmail.com>
2013-06-19 10:55:39 -0700

    change the way I run coverage tests. Get coverage of everything and filter when reporting

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-coverage run --source="/home/david/git/django_kids/src/links" manage.py test --settings=django_kids.settings.test
+coverage run manage.py test --settings=django_kids.settings.test
 coverage report --include="/home/david/git/django_kids/src/*" --omit="links/migrations/*,django_kids/settings/*,links/tests/*"
