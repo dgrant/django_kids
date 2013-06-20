e3d9aff01706aa6f919ef7de1d8874a5af682982
David Grant <davidgrant@gmail.com>
2013-06-19 23:08:29 -0700

    Added another view test. Improved test script and added .coveragerc file.

--- src/links/tests/test_views.py
+++ src/links/tests/test_views.py
@@ -8,9 +8,6 @@ from links.models import Category
 
 class TestBrowse(TestCase):
 
-    def setUp(self):
-        pass
-
     def test_non_auth(self):
         mommy.make('Link', private=False) #1
         mommy.make('Link', private=True)  #2
@@ -22,6 +19,25 @@ class TestBrowse(TestCase):
         self.assertIn('categories', resp.context)
         self.assertIn('link_list', resp.context)
 
+        self.assertEquals(set([link.pk for link in resp.context['link_list']]),
+                          set([1, 3]))
+
+    def test_auth(self):
+        authuser = User.objects.create_user('test', password='test')
+        ret = self.client.login(username='test', password='test')
+        self.assertTrue(ret)
+
+        mommy.make('Link', private=False) #1
+        mommy.make('Link', private=True)  #2
+        mommy.make('Link', private=False) #3
+        mommy.make('Link', private=False, user=authuser) #4
+
+        url = reverse('browse')
+        resp = self.client.get(url)
+        self.assertEquals(resp.status_code, 200)
+        self.assertIn('categories', resp.context)
+        self.assertIn('link_list', resp.context)
+        self.assertNotIn('Login', resp.content)
         self.assertEquals(set([link.pk for link in resp.context['link_list']]),
                           set([1, 3]))
 
@@ -47,21 +63,25 @@ class TestBrowse(TestCase):
                           set([1]))
 
 
-    def test_auth(self):
+class TestMyLinks(TestCase):
+    def test_linklist_unauth(self):
+        pass
+    
+    def test_linklist_auth(self):
         authuser = User.objects.create_user('test', password='test')
         ret = self.client.login(username='test', password='test')
         self.assertTrue(ret)
 
-        mommy.make('Link', private=False) #1
-        mommy.make('Link', private=True)  #2
+        mommy.make('Link', private=False, user=authuser) #1
+        mommy.make('Link', private=True, user=authuser)  #2
         mommy.make('Link', private=False) #3
-        mommy.make('Link', private=False, user=authuser) #4
+        mommy.make('Link', private=True) #4
 
-        url = reverse('browse')
+        url = reverse('mylinks')
         resp = self.client.get(url)
         self.assertEquals(resp.status_code, 200)
         self.assertIn('categories', resp.context)
         self.assertIn('link_list', resp.context)
-        self.assertNotIn('Login', resp.content)
         self.assertEquals(set([link.pk for link in resp.context['link_list']]),
-                          set([1, 3]))
+                          set([1, 2]))
+
