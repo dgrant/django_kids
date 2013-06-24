3af99a29e3d1e95a7acca5c102bf9c7d47b2ca0d
David Grant <davidgrant@gmail.com>
2013-06-23 22:36:13 -0700

    make links active when they are supposed to be

--- src/django_kids/settings/base.py
+++ src/django_kids/settings/base.py
@@ -98,6 +98,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
 )
 
 MIDDLEWARE_CLASSES = (
