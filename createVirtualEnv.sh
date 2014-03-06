44a608288b3e262e97a32706825d37b937b08797
David Grant <davidgrant@gmail.com>
2014-03-05 16:15:37 -0800

    removed pip and virtualenv upgrade from script

--- createVirtualEnv.sh
+++ createVirtualEnv.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 rm -rf env
-sudo easy_install pip
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
 virtualenv --distribute --no-site-packages env
 env/bin/pip install --download-cache=~/.pip-cache -r requirements.txt
