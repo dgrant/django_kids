ce03bc8e9d47b37028b28e36fc15c18fa109466a
David Grant <davidgrant@gmail.com>
2017-03-19 22:53:06 -0700

    update scripts

--- loadRemoteDb.sh
+++ loadRemoteDb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh -C david@slice mysqldump -u$1 -p$1 $1 > db.sql
+ssh -C david@linode mysqldump -u$1 -p$1 $1 > db.sql
 mysql -u$1 -p$1 $1 < db.sql
 rm db.sql
--- pushToRemoteDb.sh
+++ pushToRemoteDb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysqldump -u$1 -p$1 $1 | ssh -C david@slice mysql -u$1 -p$1 $1
+mysqldump -u$1 -p$1 $1 | ssh -C david@linode mysql -u$2 -p$2 $2
